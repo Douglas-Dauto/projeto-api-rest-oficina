d014fe0a9bf316f949ae80dd13004a92bc51a081
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-13 11:32:49 -0300

    Implementado configurações de teste e repository do user

---
+++ src/main/java/com/educandoweb/oficina/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.oficina.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.oficina.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
