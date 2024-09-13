178c1ecc9aebdf52d58e88a923ca21be1d01a055
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-13 12:30:41 -0300

    Implementado service de user

--- src/main/java/com/educandoweb/oficina/resources/UserResource.java
+++ src/main/java/com/educandoweb/oficina/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.educandoweb.oficina.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.oficina.entities.User;
+import com.educandoweb.oficina.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    UserService userService;
 
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User("douglas@gmail.com", 1L, "Douglas", "1234", "938383838");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
 
-        return ResponseEntity.ok().body(u);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
     }
 }
