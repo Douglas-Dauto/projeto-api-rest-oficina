c21282ac2c6133251d653661ce39e13dbe971436
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-09-16 12:07:45 -0300

    Implementado classe de pagamento

--- src/main/java/com/educandoweb/oficina/config/TestConfig.java
+++ src/main/java/com/educandoweb/oficina/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.oficina.entities.Category;
 import com.educandoweb.oficina.entities.Order;
 import com.educandoweb.oficina.entities.OrderItem;
+import com.educandoweb.oficina.entities.Payment;
 import com.educandoweb.oficina.entities.Product;
 import com.educandoweb.oficina.entities.User;
 import com.educandoweb.oficina.entities.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, p5.getPrice(), 2);
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 }
